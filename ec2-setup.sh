a0d18de7facd096af0ba932f06660c71b53b5097
Avijit Majumder <59084432+avijitMajumder@users.noreply.github.com>
2025-08-30 17:09:50 +0530

    Update ec2-setup.sh

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -12,6 +12,7 @@ source /home/ec2-user/myenv/bin/activate
 # Upgrade pip and install boto3
 pip install --upgrade pip
 pip install boto3
+sudo yum install git -y
 
 # Clone the GitHub repo
 cd /home/ec2-user
