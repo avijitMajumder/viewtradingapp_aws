326fd3a6cab5f2596b1c52d516f820337411984f
Avijit Majumder <59084432+avijitMajumder@users.noreply.github.com>
2025-09-01 15:51:57 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 # application.py
 from flask import Flask, render_template, request, jsonify
-from tradingview_helper import get_stock_list, load_stock_data, refresh_live_data
+from tradingview_helper import get_stock_list, load_stock_data, refresh_live_data,get_ec2_public_ip
 import os
 
 app = Flask(__name__)
@@ -33,6 +33,14 @@ def tradingview_api_stock_data():
 def refresh_data():
     result = refresh_live_data()
     return jsonify({"message": "Live data refreshed", "instruments_updated": len(result)})
+
+@app.route("/get-ec2-ip")
+def get_ec2_ip():
+    public_ip = get_ec2_public_ip()
+    if public_ip:
+        return public_ip, 200  # Return plain text, not JSON
+    else:
+        return "No running EC2 instance found", 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
